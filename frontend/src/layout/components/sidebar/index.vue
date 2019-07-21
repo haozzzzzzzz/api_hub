f8b82a4a5e6a4c76d7686792232c121084ce34c0
haozzzzzzzz <990747088@qq.com>
2019-07-21 19:56:58 +0800

    modify

--- frontend/src/layout/components/sidebar/index.vue
+++ frontend/src/layout/components/sidebar/index.vue
@@ -28,6 +28,12 @@
                     <i class="el-icon-connection"></i>
                     <span slot="title">Mock</span>
                 </el-menu-item>
+                <el-menu-item>
+                    <a class="menu-link" href="https://github.com/haozzzzzzzz/api_hub" target="_blank" ref="noopener">
+                        <i class="el-icon-service"></i>
+                        <span slot="title">Github</span>
+                    </a>
+                </el-menu-item>
             </el-menu>
         </el-scrollbar>
     </div>
@@ -95,6 +101,13 @@
             }
         }
 
+    }
+    .menu-link {
+        text-decoration: none;
+        display: inline-block;
+        width: 100%;
+        overflow: hidden;
+        color: inherit;
     }
 </style>
 <style lang="scss">

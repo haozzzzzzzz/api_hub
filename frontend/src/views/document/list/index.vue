978e43481705b6237b0b81047fe7b8ce092340cc
haozzzzzzzz <990747088@qq.com>
2019-07-21 15:50:07 +0800

    list add pagination

--- frontend/src/views/document/list/index.vue
+++ frontend/src/views/document/list/index.vue
@@ -1,60 +1,68 @@
 <template>
-    <el-table
-            :data="tableData"
-            style="width: 100%"
-            border
-    >
-        <el-table-column
-                prop="doc_id"
-                label="doc_id"
-                width="100"
-        ></el-table-column>
-
-        <el-table-column
-                label="title"
+    <div id="document-list">
+        <el-table
+                :data="tableData"
+                style="width: 100%"
+                border
         >
-            <template slot-scope="scope">
-                <el-link type="primary" :key="scope.row.doc_id" @click="openDetail(scope.row.doc_id)">{{scope.row.title}}</el-link>
-            </template>
-        </el-table-column>
+            <el-table-column
+                    prop="doc_id"
+                    label="doc_id"
+                    width="100"
+            ></el-table-column>
 
-        <el-table-column
-                prop="category_name"
-                label="category"
-                width="100"
-        ></el-table-column>
-        <el-table-column
-                prop="tags"
-                label="tags"
-                width="200"
-        >
-            <template slot-scope="scope">
-                <el-tag effect="light">tag 1</el-tag>
-                <el-tag effect="light">tag 2</el-tag>
-                <el-tag effect="light">tag 3</el-tag>
-            </template>
-        </el-table-column>
-        <el-table-column
-                prop="author_name"
-                label="author"
-                width="100"
-        ></el-table-column>
-        <el-table-column
-                prop="post_status"
-                label="status"
-                width="100"
-        ></el-table-column>
-        <el-table-column
-                prop="create_time"
-                label="time"
-                width="100"
-        ></el-table-column>
-    </el-table>
+            <el-table-column
+                    label="title"
+            >
+                <template slot-scope="scope">
+                    <el-link type="primary" :key="scope.row.doc_id" @click="openDetail(scope.row.doc_id)">{{scope.row.title}}</el-link>
+                </template>
+            </el-table-column>
+
+            <el-table-column
+                    prop="category_name"
+                    label="category"
+                    width="100"
+            ></el-table-column>
+            <el-table-column
+                    prop="tags"
+                    label="tags"
+                    width="200"
+            >
+                <template slot-scope="scope">
+                    <el-tag effect="light">tag 1</el-tag>
+                    <el-tag effect="light">tag 2</el-tag>
+                    <el-tag effect="light">tag 3</el-tag>
+                </template>
+            </el-table-column>
+            <el-table-column
+                    prop="author_name"
+                    label="author"
+                    width="100"
+            ></el-table-column>
+            <el-table-column
+                    prop="post_status"
+                    label="status"
+                    width="100"
+            ></el-table-column>
+            <el-table-column
+                    prop="create_time"
+                    label="time"
+                    width="100"
+            ></el-table-column>
+        </el-table>
+        <el-pagination
+                background
+                layout="total, prev, pager, next"
+                :total="1000">
+        </el-pagination>
+    </div>
 </template>
 
 <script>
     export default {
         name: "document_list",
+        el: '#document-list',
         data(){
             return {
                 tableData: [
@@ -91,5 +99,7 @@
 </script>
 
 <style scoped>
-
+    .el-table {
+        margin-bottom: 15px;
+    }
 </style>

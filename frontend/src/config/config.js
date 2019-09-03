9fcd525fa45ca4ed55832fc00f58577dfb3030be
luo hao <990747088@qq.com>
2019-09-03 19:33:23 +0800

    modify

--- frontend/src/config/config.js
+++ frontend/src/config/config.js
@@ -1,4 +1,4 @@
 export default {
     backendApi: 'http://127.0.0.1:18000',
-    defaultSwaggerJson: "https://petstore.swagger.io/v2/swagger.json"
+    defaultSwaggerJson: "https://raw.githubusercontent.com/haozzzzzzzz/api_hub/master/backend/swagger.json"
 }

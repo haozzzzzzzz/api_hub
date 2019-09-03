f8d386fc10ca1c708b8e57b5d1610ea7465e26eb
luo hao <990747088@qq.com>
2019-09-04 00:43:27 +0800

    modify

--- backend/swagger.sh
+++ backend/swagger.sh
@@ -1,11 +1,9 @@
 #!/usr/bin/env bash
 title="Api hub backend api"
-#api swagger -c "luohao" -d "Api hub backend api specication" -H "127.0.0.1:18000" -n ${title}
+api swagger -c "luohao" -d "Api hub backend api specication" -H "127.0.0.1:18000" -n ${title}
 
 # post swagger spec
-content="$(cat ./swagger.json)"
-body=$(python -c "import json;f=open('./swagger.json', 'r');content=f.read();reqBody = {'title': '${title}','category_id': 1,'spec_content': content};strBody=json.dumps(reqBody);print(strBody);f.close()")
-
+body=$(python3 -c "import json;f=open('./swagger.json', 'r');content=f.read();reqBody = {'title': '${title}','category_id': 1,'spec_content': content};strBody=json.dumps(reqBody);print(strBody);f.close()")
 curl -X POST \
   http://127.0.0.1:18000/api/api_hub/v1/doc/doc/check_add \
   -H 'Content-Type: application/json' \

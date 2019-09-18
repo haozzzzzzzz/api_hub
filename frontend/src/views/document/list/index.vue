f2416c95023c13c5b2b09dd40f5c976254e50f86
luo hao <990747088@qq.com>
2019-09-18 13:39:58 +0800

    search add keyup.enter.native event handler

--- frontend/src/views/document/list/index.vue
+++ frontend/src/views/document/list/index.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="tab-content">
         <div style="margin-bottom: 15px;">
-            <el-input placeholder="请输入搜索内容" v-model="search" class="input-with-select">
+            <el-input placeholder="请输入搜索内容" v-model="search" class="input-with-select" @keyup.enter.native="handleSearch">
                 <el-button slot="append" icon="el-icon-search" @click="handleSearch"></el-button>
             </el-input>
         </div>
